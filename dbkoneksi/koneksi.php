ff048f32236454b60f263f84abd7a42814ccb25b
raizaldi <achmad.raizaldi@gmail.com>
2017-03-30 00:48:49 +0700

    perubahan database koneksi to hostinger

--- dbkoneksi/koneksi.php
+++ dbkoneksi/koneksi.php
@@ -1,3 +1,4 @@
 <?php
-$db = new mysqli("localhost","root","","akua");
+//$db = new mysqli("localhost","root","","akua");
+$db = new mysqli("mysql.idhostinger.com","u276036398_akuan","amZ0KUNVG8c1","u276036398_akua");
 ?>
